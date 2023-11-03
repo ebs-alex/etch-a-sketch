166fe7ef6e52388e634ee333491e02303cec2cb6
Alex Moran <ebs.alex@icloud.com>
2023-11-02 21:34:35 -0700

    16by16 grid laid out. mousover event listener added to all cells.

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 const title = document.querySelector("h1")
 title.style.textDecoration = "underline"
+
+let gridAmount = 16
+
+const etchContainer = document.querySelector("#etch-container")
+etchContainer.style.backgroundColor = "grey"
+
+for (i = 1; i <= gridAmount ; i++) {
+    let newColumn = document.createElement("div")
+    newColumn.setAttribute("class", "column")
+
+    for (j = 1; j <= gridAmount ; j++) {
+        let newCell = document.createElement("div")
+        newCell.setAttribute("class","cell")
+        newColumn.appendChild(newCell)
+    }
+    etchContainer.appendChild(newColumn)
+}
+
+const cells = document.querySelectorAll(".cell")
+
+cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "white"
+    })
+})
