e7d94a2c17d3faf006e4aeb7e459f3d3476b6573
Alex Moran <ebs.alex@icloud.com>
2023-11-02 21:54:53 -0700

    added random color funciton when cells are hovered over. added increasing opacity when cells are hovered over multiple times.

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ for (i = 1; i <= gridAmount ; i++) {
     for (j = 1; j <= gridAmount ; j++) {
         let newCell = document.createElement("div")
         newCell.setAttribute("class","cell")
+        newCell.style.opacity = 1
         newColumn.appendChild(newCell)
     }
     etchContainer.appendChild(newColumn)
@@ -22,6 +23,17 @@ const cells = document.querySelectorAll(".cell")
 
 cells.forEach((cell) => {
     cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "white"
+        const randColor = randomColor()
+        cell.style.backgroundColor = randColor
+        cell.style.opacity -= 0.2
     })
 })
+
+function randomColor () {
+    const value1 = Math.floor(Math.random()*255)
+    const value2 = Math.floor(Math.random()*255)
+    const value3 = Math.floor(Math.random()*255)
+
+    return `rgb(${value1},${value2},${value3})`
+
+}
